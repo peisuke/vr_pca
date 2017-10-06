eb93235f2320d6237333b27c12fae66e8a50d48f
peisuke <peisuke.com@gmail.com>
2017-10-06 23:59:57 +0900

    spped test

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
+import time
 import numpy as np
 from vr_pca import *
 
-n = 100
-d = 30
+n = 10000
+d = 3000
 
 X = np.random.rand(n, d)
 
@@ -10,15 +11,19 @@ m = n
 r_h = (X **2).sum() / n
 eta = 1 / (r_h * np.sqrt(n))
 
+start_time = time.time()
 w = vr_pca(X, m, eta)
+s = time.time() - start_time
 v = np.mean(X.T.dot(X).dot(w) / w)
 
-print('VR_PCA')
-print(w)
+print('VR_PCA: %f' % s)
+print(w[:10])
 print(v)
 
+start_time = time.time()
 u_, w_, _ = np.linalg.svd(X.T.dot(X))
+s = time.time() - start_time
 
-print('SVD')
-print(u_[:,0])
+print('SVD: %f' %s)
+print(u_[:,0][:10])
 print(w_[0])
