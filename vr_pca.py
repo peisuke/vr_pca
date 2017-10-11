1b8713b6dea92e341c212df82b3172768663e9b8
peisuke <peisuke.com@gmail.com>
2017-10-11 13:08:12 +0900

    replace list

--- vr_pca.py
+++ vr_pca.py
@@ -9,17 +9,16 @@ def vr_pca(X, m, eta, rate=1e-5):
     for s in range(10):
         u_t = X.T.dot(X.dot(w_t)) / n
 
-        w = []
-        w.append(w_t)
+        w = w_t
 
         for t in range(m):
             i = np.random.randint(n)
-            _w = w[t] + eta * (X[i] * (X[i].T.dot(w[t]) - X[i].T.dot(w_t)) + u_t)
+            _w = w + eta * (X[i] * (X[i].T.dot(w) - X[i].T.dot(w_t)) + u_t)
             _w = _w / np.linalg.norm(_w)
-            w.append(_w)
+            w = _w
 
-        d = np.linalg.norm(w_t - w[-1])
-        w_t = w[-1]
+        d = np.linalg.norm(w_t - w)
+        w_t = w
 
         if d < rate:
             return w_t
